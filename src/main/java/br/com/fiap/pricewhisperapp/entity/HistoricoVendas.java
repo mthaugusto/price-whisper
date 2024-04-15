ebc788b32d55ff87665fb72a7fc8da60522a0b6a
mthaugusto <mthaugusto94@icloud.com>
2024-04-15 15:57:48 -0300

    Corrigindo o construtor

--- src/main/java/br/com/fiap/pricewhisperapp/entity/HistoricoVendas.java
+++ src/main/java/br/com/fiap/pricewhisperapp/entity/HistoricoVendas.java
@@ -2,12 +2,16 @@ package br.com.fiap.pricewhisperapp.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 @Entity
